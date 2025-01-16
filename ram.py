31e94dc89117925ced0ef4acd638f91e35d2868e
Samer-Ismael <samerismaeelbat2@hotmail.com>
2025-01-16 23:51:17 +0100

    saving

--- ram.py
+++ ram.py
@@ -12,30 +12,15 @@ def get_ram_metrics():
         "free": round(ram.available / (1024 ** 3), 2),
     }
 
-def get_rammap_path():
-    """Returns the full path to RAMMap.exe inside the lib folder."""
-    # Get the current directory where the script is running
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the 'lib' folder where RAMMap.exe is stored
-    rammap_path = os.path.join(current_dir, 'lib', 'RAMMap.exe')
-    
-    return rammap_path
-
 def clear_cache_mem():
     
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the 'lib' folder where RAMMap.exe is stored
     rammap_path = os.path.join(current_dir, 'lib', 'RAMMap.exe')
     
     
     if os.path.exists(rammap_path):
             try:
-                # Run the RAMMap command to clear the standby list
                 subprocess.run([rammap_path, '-Et'], check=True)
                 print("Standby list cleared successfully!")
             except subprocess.CalledProcessError as e:
                 print(f"Error clearing standby list: {e}")
-            else:
-                print("RAMMap executable not found at the specified path.")    
-
-        
